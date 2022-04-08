7b502a00206f3e3022d9c373af8d0a71e8f91eb7
Filip Pedersen <filip.pedersen98@gmail.com>
2022-04-08 10:47:29 +0200

    axios

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>Hello World</div>
+  <h1>Hello World</h1>
   <div class="nav">
     <ul>
-      <li>Home</li>
-      <li>About</li>
+      <li><a href="#">Home</a></li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
+axios.get("http://localhost:1337/api/team-members").then((response) => {
+  console.log(response);
+});
+
 export default {
   name: "Home-view",
 };
@@ -16,13 +21,19 @@ export default {
 
 <style>
 .nav {
+  padding-top: 2rem;
 }
 
 ul {
   list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: inline;
 }
 
 li {
   text-decoration: none;
+  display: inline;
+  padding: 2rem;
 }
 </style>

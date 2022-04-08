aad3bd6e24bc064b95833e01d462ece3753a9054
Filip Pedersen <filip.pedersen98@gmail.com>
2022-04-08 10:33:00 +0200

    created

---
+++ src/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>About Strapi</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About-view",
+};
+</script>
+
+<style></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>Hello World</div>
+  <div class="nav">
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home-view",
+};
+</script>
+
+<style>
+.nav {
+}
+
+ul {
+  list-style-type: none;
+}
+
+li {
+  text-decoration: none;
+}
+</style>
